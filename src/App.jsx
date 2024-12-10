a2ffc40d9ea48123b92c9f8217ee7fa6dfd199ba
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-10 14:20:00 +0700

    moved away from the rendering because it's laggy

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import { motion } from "motion/react"
 import Text from "./Text.jsx";
 function App() {
   const [results, setResults] = useState("");
-
+  const [inputText, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
     const fetchAIResponse = async (e) => {
       e.preventDefault();
       console.log(e)
       try {
         const genAI = new GoogleGenerativeAI("AIzaSyCwIq3Z0liDWLF2J1AF5waP87Mn0Rt2FSw");
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+        setInput("")
         const response = await model.generateContent(e.target[0].value);
         const newText = response.response.text();
 
@@ -25,12 +28,13 @@ function App() {
   return (
     <div className="w-screen h-screen bg-black">
       <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <div className="bg-gray-800 w-[1600px] h-[600px] flex flex-col justify-start items-center rounded-xl">
+        <div className="bg-gray-800 w-[1600px] h-[90%] flex flex-col justify-start items-center rounded-xl">
           <div className="w-full h-full ustify-center items-start flex flex-col rounded-xl overflow-auto space-y-5 px-10 py-10">
             {Object.values(results).map((text, index) => ( <Text result={text} />))}
           </div>
+          <form className="w-full" onSubmit={fetchAIResponse}><input type="text" value={inputText} onChange={handleChange} placeholder="Enter text" className="bg-gray-600 text-left text-4xl text-white w-full h-24 rounded-xl"/></form>
         </div>
-        <form className="w-1/2" onSubmit={fetchAIResponse}><input  className="bg-gray-600 text-center text-4xl text-white w-full h-24 rounded-xl"/></form>
+       
       </div>
     </div>
   );
--- src/Text.jsx
+++ src/Text.jsx
@@ -1,20 +1,16 @@
-import { motion } from "motion/react"
+import { motion } from "framer-motion"
+import { useEffect, useState } from "react"
+
 function Text({ result }) {
-    const renderText = (text, textSize = 'text-xl') => (
-        text.split('').map((char, index) => (
-          <motion.span
-            key={index}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { delay: index * 0.005, ease: 'easeOut', duration: 1 } }}
-            viewport={{ once: true }}
-            className={`text-white font-sans font-thin mx-auto text-center w-full ${textSize}`}
-          >
-            {char}
-          </motion.span>
-        ))
-      );
-    return (<>
-    <div className="inline-flex bg-white bg-opacity-20 rounded-2xl pb-2 px-2"><motion.p className="text-3xl font-thin text-white">{renderText(result)}</motion.p></div>
-    </>)
-    } 
-export default Text
+
+
+  return (
+    <motion.div initial={{height: 0}} animate={{height: "auto", transition: {duration: 0.5, ease:"linear"}}} className="inline-flex bg-white bg-opacity-20 rounded-2xl pb-2 px-2">
+      <motion.p initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 0.5, duration: 0.5}}}  className="text-3xl font-thin text-white">
+        {result}
+      </motion.p>
+    </motion.div>
+  );
+}
+
+export default Text;
