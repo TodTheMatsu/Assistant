02435f8972f7ca6f29067858a98136253a677adc
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-10 14:20:59 +0700

    style changes

--- src/Text.jsx
+++ src/Text.jsx
@@ -2,11 +2,9 @@ import { motion } from "framer-motion"
 import { useEffect, useState } from "react"
 
 function Text({ result }) {
-
-
   return (
     <motion.div initial={{height: 0}} animate={{height: "auto", transition: {duration: 0.5, ease:"linear"}}} className="inline-flex bg-white bg-opacity-20 rounded-2xl pb-2 px-2">
-      <motion.p initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 0.5, duration: 0.5}}}  className="text-3xl font-thin text-white">
+      <motion.p initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 0.75, duration: .75}}}  className="text-3xl font-thin text-white">
         {result}
       </motion.p>
     </motion.div>
